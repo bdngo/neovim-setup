74de4b218dfa45f100e412b66f910fc1a0ab7305
Bryan Ngo <bryanngo@berkeley.edu>
2024-08-10 11:19:57 -0700

    init setup

---
+++ lua/config/settings.lua
@@ -0,0 +1,21 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.cmd("autocmd InsertEnter * :set norelativenumber")
+vim.cmd("autocmd InsertLeave * :set relativenumber")
+
+vim.g.have_nerd_font = true
