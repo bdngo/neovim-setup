68a0b3c1027d11f56976172b2ae4b75c28c69a9f
Bryan Ngo <bngo2@apple.com>
2025-10-24 11:13:26 -0700

    fix nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,88 +1,45 @@
 return {
-	"hrsh7th/nvim-cmp",
-	version = false, -- last release is way too old
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-	},
-	-- Not all LSP servers add brackets when completing a function.
-	-- To better deal with this, LazyVim adds a custom option to cmp,
-	-- that you can configure. For example:
-	--
-	-- ```lua
-	-- opts = {
-	--   auto_brackets = { "python" }
-	-- }
-	-- ```
-	opts = function()
-		-- Register nvim-cmp lsp capabilities
-		vim.lsp.config("*", { capabilities = require("cmp_nvim_lsp").default_capabilities() })
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
-		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-		local cmp = require("cmp")
-		local defaults = require("cmp.config.default")()
-		local auto_select = true
-		return {
-			auto_brackets = {}, -- configure any filetype to auto add brackets
-			completion = {
-				completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
-			},
-			preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
-			mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
-				["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
-				["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				["<C-CR>"] = function(fallback)
-					cmp.abort()
-					fallback()
-				end,
-				["<tab>"] = function(fallback)
-					return LazyVim.cmp.map({ "snippet_forward", "ai_nes", "ai_accept" }, fallback)()
-				end,
-			}),
-			sources = cmp.config.sources({
-				{ name = "lazydev" },
-				{ name = "nvim_lsp" },
-				{ name = "path" },
-			}, {
-				{ name = "buffer" },
-			}),
-			formatting = {
-				format = function(entry, item)
-					local icons = LazyVim.config.icons.kinds
-					if icons[item.kind] then
-						item.kind = icons[item.kind] .. item.kind
-					end
+        require("luasnip.loaders.from_vscode").lazy_load()
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+            }, {
+                { name = "buffer" },
+            }),
+        })
 
-					local widths = {
-						abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-						menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-					}
-
-					for key, width in pairs(widths) do
-						if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-							item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
-						end
-					end
-
-					return item
-				end,
-			},
-			experimental = {
-				-- only show ghost text when we show ai completions
-				ghost_text = vim.g.ai_cmp and {
-					hl_group = "CmpGhostText",
-				} or false,
-			},
-			sorting = defaults.sorting,
-		}
-	end,
-	main = "lazyvim.util.cmp",
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        vim.lsp.config("lua_ls", capabilities)
+        vim.lsp.config("rust_analyzer", capabilities)
+        vim.lsp.config("ruff", capabilities)
+        vim.lsp.config("pylsp", capabilities)
+    end,
 }
